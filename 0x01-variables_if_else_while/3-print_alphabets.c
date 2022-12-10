b16eefc592ef5b8cceae41825cdae04a05efd5a3
Victor Adewoye <adewoyevictoradedeji@gmail.com>
2022-12-09 17:16:16 -0800

    Prints upper and lower alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ * Return: Always 0
+ */
+int main(void)
+{
+	char c;
+
+	char d;
+
+	c = 'a';
+	d = 'A';
+	while
+		(c <= 'z') {
+			putchar(c);
+			c++;
+		}
+	while
+		(d <= 'Z') {
+			putchar(c);
+			d++;
+		}
+	putchar('\n');
+	return (0);
+}
